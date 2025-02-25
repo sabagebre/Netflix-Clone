7723e9eb977553334fc2a8996a92ba2fb03605ff
sabagebre <gebresaba467@gmail.com>
2025-02-25 14:39:16 -0700

    banner component updated

--- netflix-clone/src/components/Banner/Banner.jsx
+++ netflix-clone/src/components/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-// import React from 'react'
+2// import React from 'react'
 
 import { useEffect, useState } from "react";
 import axios from "../../Utils/axios";
@@ -63,3 +63,4 @@ const Banner = () => {
 };
 
 export default Banner;
+
